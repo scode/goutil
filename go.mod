05317cd8bc92e7f118666b4d3a845c3c65126e2c
Peter Schuller <peter.schuller@infidyne.com>
2018-08-10 00:26:12 -0700

    Switch to vgo, testify. Add linters. (#1)

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/scode/goutil
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/magiconair/properties v1.8.0
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+)
--- mathutil/minmax_test.go
+++ mathutil/minmax_test.go
@@ -1,147 +1,78 @@
 package mathutil
 
-import "testing"
+import (
+	"testing"
+	"github.com/magiconair/properties/assert"
+)
 
 func TestMinInt8(t *testing.T) {
-	if MinInt8(1, 2) != 1 {
-		t.Fail()
-	}
-	if MinInt8(2, 1) != 1 {
-		t.Fail()
-	}
-	if MinInt8(1, 1) != 1 {
-		t.Fail()
-	}
+	assert.Equal(t, int8(1), MinInt8(1, 2))
+	assert.Equal(t, int8(1), MinInt8(2, 1))
+	assert.Equal(t, int8(1), MinInt8(1, 1))
 }
 
 func TestMaxInt8(t *testing.T) {
-	if MaxInt8(1, 2) != 2 {
-		t.Fail()
-	}
-	if MaxInt8(2, 1) != 2 {
-		t.Fail()
-	}
-	if MaxInt8(2, 2) != 2 {
-		t.Fail()
-	}
+	assert.Equal(t, int8(2), MaxInt8(1, 2))
+	assert.Equal(t, int8(2), MaxInt8(2, 1))
+	assert.Equal(t, int8(2), MaxInt8(2, 2))
 }
 
 func TestMinUint8(t *testing.T) {
-	if MinUint8(1, 2) != 1 {
-		t.Fail()
-	}
-	if MinUint8(2, 1) != 1 {
-		t.Fail()
-	}
-	if MinUint8(1, 1) != 1 {
-		t.Fail()
-	}
+	assert.Equal(t, uint8(1), MinUint8(1, 2))
+	assert.Equal(t, uint8(1), MinUint8(2, 1))
+	assert.Equal(t, uint8(1), MinUint8(1, 1))
 }
 
 func TestMaxUint8(t *testing.T) {
-	if MaxUint8(1, 2) != 2 {
-		t.Fail()
-	}
-	if MaxUint8(2, 1) != 2 {
-		t.Fail()
-	}
-	if MaxUint8(2, 2) != 2 {
-		t.Fail()
-	}
+	assert.Equal(t, uint8(2), MaxUint8(1, 2))
+	assert.Equal(t, uint8(2), MaxUint8(2, 1))
+	assert.Equal(t, uint8(2), MaxUint8(2, 2))
 }
 
 func TestMinInt32(t *testing.T) {
-	if MinInt32(1, 2) != 1 {
-		t.Fail()
-	}
-	if MinInt32(2, 1) != 1 {
-		t.Fail()
-	}
-	if MinInt32(1, 1) != 1 {
-		t.Fail()
-	}
+	assert.Equal(t, int32(1), MinInt32(1, 2))
+	assert.Equal(t, int32(1), MinInt32(2, 1))
+	assert.Equal(t, int32(1), MinInt32(1, 1))
 }
 
 func TestMaxInt32(t *testing.T) {
-	if MaxInt32(1, 2) != 2 {
-		t.Fail()
-	}
-	if MaxInt32(2, 1) != 2 {
-		t.Fail()
-	}
-	if MaxInt32(2, 2) != 2 {
-		t.Fail()
-	}
+	assert.Equal(t, int32(2), MaxInt32(1, 2))
+	assert.Equal(t, int32(2), MaxInt32(2, 1))
+	assert.Equal(t, int32(2), MaxInt32(2, 2))
 }
 
 func TestMinUint32(t *testing.T) {
-	if MinUint32(1, 2) != 1 {
-		t.Fail()
-	}
-	if MinUint32(2, 1) != 1 {
-		t.Fail()
-	}
-	if MinUint32(1, 1) != 1 {
-		t.Fail()
-	}
+	assert.Equal(t, uint32(1), MinUint32(1, 2))
+	assert.Equal(t, uint32(1), MinUint32(2, 1))
+	assert.Equal(t, uint32(1), MinUint32(1, 1))
 }
 
 func TestMaxUint32(t *testing.T) {
-	if MaxUint32(1, 2) != 2 {
-		t.Fail()
-	}
-	if MaxUint32(2, 1) != 2 {
-		t.Fail()
-	}
-	if MaxUint32(2, 2) != 2 {
-		t.Fail()
-	}
+	assert.Equal(t, uint32(2), MaxUint32(1, 2))
+	assert.Equal(t, uint32(2), MaxUint32(2, 1))
+	assert.Equal(t, uint32(2), MaxUint32(2, 2))
 }
 
 func TestMinInt64(t *testing.T) {
-	if MinInt64(1, 2) != 1 {
-		t.Fail()
-	}
-	if MinInt64(2, 1) != 1 {
-		t.Fail()
-	}
-	if MinInt64(1, 1) != 1 {
-		t.Fail()
-	}
+	assert.Equal(t, int64(1), MinInt64(1, 2))
+	assert.Equal(t, int64(1), MinInt64(2, 1))
+	assert.Equal(t, int64(1), MinInt64(1, 1))
 }
 
 func TestMaxInt64(t *testing.T) {
-	if MaxInt64(1, 2) != 2 {
-		t.Fail()
-	}
-	if MaxInt64(2, 1) != 2 {
-		t.Fail()
-	}
-	if MaxInt64(2, 2) != 2 {
-		t.Fail()
-	}
+	assert.Equal(t, int64(2), MaxInt64(1, 2))
+	assert.Equal(t, int64(2), MaxInt64(2, 1))
+	assert.Equal(t, int64(2), MaxInt64(2, 2))
 }
 
 func TestMinUint64(t *testing.T) {
-	if MinUint64(1, 2) != 1 {
-		t.Fail()
-	}
-	if MinUint64(2, 1) != 1 {
-		t.Fail()
-	}
-	if MinUint64(1, 1) != 1 {
-		t.Fail()
-	}
+	assert.Equal(t, uint64(1), MinUint64(1, 2))
+	assert.Equal(t, uint64(1), MinUint64(2, 1))
+	assert.Equal(t, uint64(1), MinUint64(1, 1))
 }
 
 func TestMaxUint64(t *testing.T) {
-	if MaxUint64(1, 2) != 2 {
-		t.Fail()
-	}
-	if MaxUint64(2, 1) != 2 {
-		t.Fail()
-	}
-	if MaxUint64(2, 2) != 2 {
-		t.Fail()
-	}
+	assert.Equal(t, uint64(2), MaxUint64(1, 2))
+	assert.Equal(t, uint64(2), MaxUint64(2, 1))
+	assert.Equal(t, uint64(2), MaxUint64(2, 2))
 }
